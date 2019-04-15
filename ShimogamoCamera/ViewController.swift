e6e704c5ba259b5faaf216d608ead92a7d291d85
“sangatsunijuku” <yusukekubotokyo@gmail.com>
2019-04-15 18:27:01 +0900

    Initial Commit

---
+++ ShimogamoCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShimogamoCamera
+//
+//  Created by sangatsunijuku on 2019/04/15.
+//  Copyright © 2019年 sangatsunijuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
